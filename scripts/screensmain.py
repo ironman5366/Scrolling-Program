7c6366a4f1fd7d6a6d6b07c83aed8f9b76a38ff1
ironman5366 <will.beddow@gmail.com>
2015-12-08 13:10:31 -0500

    Switched to thread module and it works, now work on having a webview window in the main thread

--- scripts/screensmain.py
+++ scripts/screensmain.py
@@ -3,10 +3,15 @@ import scripts.settings as settings
 from multiprocessing import Pool
 import webview
 import time
-def callback():
-    print "Callback"
-    webview.load_url("google.com")
+import thread
+status=True
 class main():
+    def pausebox():
+        pb=easygui.buttonbox(title="Wall Program", msg="", choices="Play/Pause")
+        if status==False:
+            status=True
+        elif status==True:
+            status=False
     def counter(self, duration):
         num=0
         print duration
@@ -16,21 +21,15 @@ class main():
             num+=(0.1)
             print num
             if duration == num or num > duration:
-                break
                 return "finished"
             else:
-                f=open("program-files/status.txt").read()
-                if 'pause' in f:
-                    f.close()
+                if status==False:
                     while True:
                         time.sleep(0.1)
-                        f=open("status.txt").read()
-                        if "pause" in f:
+                        if f==False:
                             pass
                         else:
                             break
-                        f.close()
-
 
     def viewer(self,url):
         print "In viewer"
@@ -42,31 +41,25 @@ class main():
             print "Loading url"
             webview.load_url(url)
     def dayview(self, duration):
-        print 'In dayview'
-        print "Starting webview"
-        self.viewer("https://calendar.google.com/?mode=day")
-        pool = Pool(processes=1)              # Start a worker processes.
-        result = pool.apply_async(self.counter,duration, callback) # Evaluate "f(10)" asynchronously calling callback when finished.
-    def weekview(self, duration):
-        pool = Pool(processes=1)              # Start a worker processes.
-        result = pool.apply_async(self.viewer, ["https://calendar.google.com/?mode=week"], callback) # Evaluate "f(10)" asynchronously calling callback when finished.
-        countvar=self.counter(duration)
+        thread.start_new_thread ( self.viewer, ("https://calendar.google.com/?mode=day",))
+        self.counter(duration)
+    def weekview(self, duration): 
+        thread.start_new_thread ( self.viewer, ("https://calendar.google.com/?mode=week",))
+        self.counter(duration)
     def weather(self, duration):
-        pool = Pool(processes=1)              # Start a worker processes.
-        result = pool.apply_async(self.viewer, ["html/weather.html"], callback) # Evaluate "f(10)" asynchronously calling callback when finished.
-        countvar=self.counter(duration)
+        thread.start_new_thread ( self.viewer, ("html/weather.html",))
+        self.counter(duration)
     def timescreen(self, duration):
-        pool = Pool(processes=1)              # Start a worker processes.
-        result = pool.apply_async(self.viewer, ["html/timescreen.html"], callback) # Evaluate "f(10)" asynchronously calling callback when finished.
-        countvar=self.counter(duration)
+        thread.start_new_thread ( self.viewer, ("html/timescreen.html",))
+        self.counter(duration)
     def alarm(self, duration):
-        pool = Pool(processes=1)              # Start a worker processes.
-        result = pool.apply_async(self.viewer, ["html/alarm.html"], callback) # Evaluate "f(10)" asynchronously calling callback when finished.
-        countvar=self.counter(duration)
+        thread.start_new_thread ( self.viewer, ("html/alarm.html",))
+        self.counter(duration)
     def start(self):
         def get_duration(screen_name):
             screen_duration = int(settings.main().get_setting(screen_name))
             return screen_duration
+        thread.start_new_thread ( self.pausebox, () )
         self.dayview(get_duration("dayview"))
         self.weekview(get_duration("weekview"))
         self.weather(get_duration("weather"))
