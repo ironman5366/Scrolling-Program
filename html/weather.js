1d5cee2227956f13b04ced7852a026a9d7f266c2
Will Beddow <will.beddow@gmail.com>
2015-12-12 16:08:13 -0500

    html/css edits, now all thats left is fullscreen

--- html/weather.js
+++ html/weather.js
@@ -31,7 +31,8 @@ function loadWeather(location, woeid) {
     woeid: woeid,
     unit: 'f',
     success: function(weather) {
-      html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+    //'<h2><i class="icon-' +weather.code + '"></i> ' +
+      html ='<h2>'+ weather.temp + '&deg;' + weather.units.temp + '</h2>';
       html += '<ul><li>' + weather.city + ', ' + weather.region + '</li>';
       html += '<li class="currently">' + weather.currently + '</li>';
       html += '<li>' + weather.alt.temp + '&deg;C</li></ul>';
